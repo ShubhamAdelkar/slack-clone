4c30a8e4aca6be71e854bf930882cf4f6ebe4b0f
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-26 15:48:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
+
+function App() {
+  return (
+    <div>
+      <h1>Shubham Adelkar</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* React Router */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-left">{/* avatars */}</div>
+      <div className="header-search">{/* search-icon */}</div>
+      <div className="header-right">{/* help-icon */}</div>
+    </div>
+  );
+}
