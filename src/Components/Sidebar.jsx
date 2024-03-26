98b81f0a085f825b0dbd5350a4779010a9425aa6
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-26 19:37:48 +0530

    update sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className=" bg-[#4A154B] text-white flex border-t border-[#592f5b] w-[260px] p-4 justify-between">
+      <div className="">
+        <h2 className="">Shubham Adelkar</h2>
+        <div className="flex flex-row">
+          <FiberManualRecordIcon className="text-[green] scale-75" />
+          <h3 className="">imbachhu</h3>
+        </div>
+      </div>
+      <ModeEditIcon className="scale-75" />
+    </div>
+  );
 }
